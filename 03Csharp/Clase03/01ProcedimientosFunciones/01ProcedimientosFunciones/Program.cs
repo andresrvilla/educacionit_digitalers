a634b6bd8e3c0425d4400eb23069fb125e43d242
Andrés Villa <robertoavilla@gmail.com>
2020-10-21 22:06:57 -0300

    Clase 3 CSharp

---
+++ 03Csharp/Clase03/01ProcedimientosFunciones/01ProcedimientosFunciones/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01ProcedimientosFunciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroUno = CapturarDato();
+            int numeroDos = CapturarDato();
+            int resultadoSuma = Sumar(numeroUno, numeroDos);
+            InformarResultado(resultadoSuma);
+            Console.ReadKey();
+        }
+
+        static int CapturarDato()
+        {
+            Console.Write("Por favor ingrese un numero para sumar: ");
+            string datoIngresado = Console.ReadLine();
+            int datoConvertido = Convert.ToInt32(datoIngresado);
+            return datoConvertido;
+        }
+
+        static int Sumar(int numeroUno,int numeroDos)
+        {
+            int resultado = numeroUno + numeroDos;
+            return resultado;
+        }
+
+        static void InformarResultado(int resultado)
+        {
+            Console.WriteLine("El resultado de la suma es " + resultado);
+        }
+    }
+}
---
+++ 03Csharp/Clase03/02AmbitoVariablesFunciones/02AmbitoVariablesFunciones/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02AmbitoVariablesFunciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorNumerico = 10;
+            int otroValorNumerico = 300;
+            Console.WriteLine("El valor de valorNumerico es " + valorNumerico);
+            ModificarValor(valorNumerico);
+            Console.WriteLine("El valor de valorNumerico es " + valorNumerico);
+            ModificarValorPorReferencia(ref valorNumerico);
+            ModificarValorPorReferencia(ref otroValorNumerico);
+            Console.WriteLine("El valor de valorNumerico es " + valorNumerico);
+            Console.ReadKey();
+        }
+
+        static void ModificarValor(int valor)
+        {
+            valor = 100;
+            Console.WriteLine("El valor de valor es " + valor);
+        }
+
+        static void ModificarValorPorReferencia(ref int valor)
+        {
+            valor = 100;
+            Console.WriteLine("El valor de valor es " + valor);
+        }
+    }
+}
