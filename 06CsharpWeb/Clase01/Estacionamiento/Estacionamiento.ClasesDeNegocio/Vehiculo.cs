57b68f09d8cbf1783fe485b66760f8e549f10d13
Andrés Villa <robertoavilla@gmail.com>
2020-12-02 21:53:16 -0300

    Clase 1 ASP.NET

---
+++ 06CsharpWeb/Clase01/Estacionamiento/Estacionamiento.ClasesDeNegocio/Vehiculo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamiento.ClasesDeNegocio
+{
+    public abstract class Vehiculo
+    {
+        public string Patente { get; protected set; }
+
+        public abstract decimal Precio { get; }
+    }
+}
---
+++ 06CsharpWeb/Clase01/Estacionamiento/Estacionamiento.Consola/Program.cs
@@ -0,0 +1,18 @@
+﻿using Estacionamiento.ClasesDeNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamiento.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto miAuto = new Auto("XXXXXXX");
+            Console.ReadKey();
+        }
+    }
+}
